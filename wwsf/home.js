dae3010f7950ce46504eb1bb1c996de3c7bb3a91
pentagram-nj <jaeyoon.kim@pentagram.com>
2014-09-12 17:48:47 -0400

    1

--- wwsf/home.js
+++ wwsf/home.js
@@ -448,6 +448,47 @@ google.maps.event.addListener(marker_six_second, 'click', function() {
       });
 
 
+          $(function() {
+            $('#slides').superslides({
+              hashchange: true,
+              play: 2500,
+              inherit_width_from: '.wide-container',
+              inherit_height_from: '.wide-container'
+            });
 
+            $('#slides').on('mouseenter', function() {
+              $(this).superslides('stop');
+              console.log('Stopped')
+            });
+            $('#slides').on('mouseleave', function() {
+              $(this).superslides('start');
+              console.log('Started')
+            });
+          });
+        </script>
+
+        <script>
+
+        function sound_return(){
+          var sound_array = ["sound1.mp3", "sound2.mp3"];
+          var sound = sound_array[Math.floor(Math.random() * sound_array.length)];
+          return sound;
+        }
+
+        var audioElement = document.createElement('audio');
+        audioElement.addEventListener("load", function() {
+            audioElement.play();
+            btn.hide();
+        }, true);
+        audioElement.addEventListener("ended", function() {
+            btn.show();
+        }, true);
+
+
+        var btn = $('#sound').click(function(event){
+            event.preventDefault();
+            audioElement.setAttribute('src', 'sound/'+sound_return());
+            audioElement.load();
+        });
 
 
